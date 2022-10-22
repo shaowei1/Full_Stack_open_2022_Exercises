fca9010ad93174baf6c3c3c63ed18d1d38785fa5
shaowei <shaowei@infimind.com>
2022-10-22 21:30:24 +0800

    1.5: course information step5

--- part1/1.2-course-information/src/App.js
+++ part1/1.2-course-information/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-      <div><h1>{props.course}</h1></div>
+      <div><h1>{props.course.name}</h1></div>
   )
   
 }
@@ -16,7 +16,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  const [p1, p2, p3] = props.parts
+  const [p1, p2, p3] = props.course.parts
   return (
     <div>
       <Part part={p1.name} exercises={p1.exercises} />
@@ -27,7 +27,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  const [p1, p2, p3] = props.parts
+  const [p1, p2, p3] = props.course.parts
   return (
     <div>
       <p>Number of exercises {p1.exercises + p1.exercises + p1.exercises}</p>
@@ -36,26 +36,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
       <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
