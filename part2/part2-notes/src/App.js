a689ee87a4994a8d367a2e7bbfb77c9bedb35e17
shaowei <shaowei_8910@163.com>
2022-12-01 15:15:35 +0800

    2.11: The Phonebook Step6 Task Solution * use axios-library get data from server Note

--- part2/part2-notes/src/App.js
+++ part2/part2-notes/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Person from "./components/Person";
 
 const Persons = (prop) => (
@@ -31,16 +32,17 @@ const Filter = (prop) => (
 );
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [queryName, setQueryName] = useState("");
 	const [newPhoneNumber, setnewPhoneNumber] = useState("");
-
+	useEffect(() => {
+		console.log("effect");
+		axios.get("http://localhost:3001/persons").then((response) => {
+			console.log("promise fulfilled");
+			setPersons(response.data);
+		});
+	}, []);
 	const addPerson = (event) => {
 		event.preventDefault();
 		console.log("button clicked", event.target);
